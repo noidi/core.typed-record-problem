f3d2409e9d933fce860448d5a950ed23bc2b5ee3
Timo Mihaljov <timo.mihaljov@solita.fi>
2013-09-04 11:40:16 +0300

    Add ann-protocol & ann-record

--- src/problem/core.clj
+++ src/problem/core.clj
@@ -1,9 +1,13 @@
 (ns problem.core
   (:require [clojure.core.typed :as t]))
 
+(t/ann-protocol MyProto
+ foo
+ [MyProto -> nil])
 (defprotocol MyProto
   (foo [my-proto]))
 
+(t/ann-record MyRec [])
 (defrecord MyRec []
   MyProto
   (foo [my-rec] nil))
